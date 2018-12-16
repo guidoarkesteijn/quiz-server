4d5586f81a7b4b9bbdcf2bd3513a3718f8b1a157
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2018-12-16 13:26:29 +0100

    Adding basic main.go and added dockerfile

--- main.go
+++ main.go
@@ -6,14 +6,29 @@ import (
 	"github.com/twinj/uuid"
 
 	Data "guido.arkesteijn/quiz-server/data"
+	Question "guido.arkesteijn/quiz-server/data/Question"
 )
 
 func main() {
-	baseMessage := Data.BaseMessage{}
+	fmt.Println("Starting server")
+
+	baseMessage := Data.BaseMessage{Id: 1345678}
 	fmt.Println(baseMessage.Id)
 
 	u := uuid.NewV4()
-	question := Data.Question{Guid: u.String()}
+
+	var answer1 = Question.Answer{Text: "Answer 1"}
+	var answer2 = Question.Answer{Text: "Answer 2"}
+	var answer3 = Question.Answer{Text: "Answer 3"}
+	var answer4 = Question.Answer{Text: "Answer 4"}
+
+	var answers = []*Question.Answer{&answer1, &answer2, &answer3, &answer4}
+	question := Question.Question{Guid: u.String(), Answers: answers}
 
 	fmt.Println(question.Guid)
+	for index := 0; index < len(question.Answers); index++ {
+		fmt.Println(question.Answers[index].Text)
+	}
+
+	fmt.Println("Ended server")
 }
